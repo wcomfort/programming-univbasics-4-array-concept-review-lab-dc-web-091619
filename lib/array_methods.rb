c21eb1cda934c8e6975b39c6de6b4b66ae67da26
Will Comfort <comfortwb21@gmail.com>
2019-09-03 14:58:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,14 +6,14 @@ end
 
 def find_max_value(array)
   array.length.times { |index|
-  puts array.max
+  return array.max
   }
 end
 
 def find_min_value(array)
  counter = 0 
 while counter < array.length do
- puts array.min
+ return array.min
  counter += 1
 end
 end 
