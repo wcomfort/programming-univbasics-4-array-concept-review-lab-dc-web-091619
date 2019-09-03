7b4390e76a8f6dc86e685adb7f4eb9ca91b89907
Will Comfort <comfortwb21@gmail.com>
2019-09-03 14:40:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,19 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times { |index|
+  puts array[index]
+}
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.length.times { |index|
+  puts array.max
+  }
 end
 
 def find_min_value(array)
-  # Add your solution here
+ counter = 0 
+while counter < array.length do
+ puts array.min
+ counter += 1
 end
+
