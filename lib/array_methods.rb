a8e7173949492f05a552133d388fe7c2568e21de
Will Comfort <comfortwb21@gmail.com>
2019-09-03 15:05:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-  return value_to_find == array[index]
+  if value_to_find == array[index]
+    return index 
 end
 end
 
