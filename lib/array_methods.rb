0be5315cbf6a917baa0371c0ea5151f958189709
Will Comfort <comfortwb21@gmail.com>
2019-09-03 15:04:25 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |index|
+  array.length.times do |index|
   return value_to_find == array[index]
-}
+end
 end
 
 def find_max_value(array)
